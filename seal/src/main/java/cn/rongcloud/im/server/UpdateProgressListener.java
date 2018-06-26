67794217a087774912f265c599686d26b2213bde
yangdengxueshi <1437977371@qq.com>
2018-06-26 10:15:56 +0800

    项目初始化!

---
+++ seal/src/main/java/cn/rongcloud/im/server/UpdateProgressListener.java
@@ -0,0 +1,26 @@
+package cn.rongcloud.im.server;
+
+
+public interface UpdateProgressListener {
+    /**
+     * download start
+     */
+    void start();
+
+    /**
+     * update download progress
+     *
+     * @param progress
+     */
+    void update(int progress);
+
+    /**
+     * download success
+     */
+    void success();
+
+    /**
+     * download error
+     */
+    void error();
+}
